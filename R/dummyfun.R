0606a3955b204acd7fec5fd8fae0282e51c867be
Michal Bojanowski <michal2992@gmail.com>
2020-01-07 00:07:02 +0100

    Add dummyfun()

---
+++ R/dummyfun.R
@@ -0,0 +1,14 @@
+#' Dummy function
+#'
+#' This is a dummy function.
+#'
+#' @param x a dummy numeric argument
+#'
+#' @return Value of `x` increased by 2.
+#'
+#' @export
+#'
+#' @examples
+#' dummyfun(2)
+
+dummyfun <- function(x) x + 2
